4c4ff1d0a8927023d645ed1e86f711ca56edafb5
stzekov <stanislav.tzekov@dxc.com>
2024-09-17 21:37:57 +0300

    Render latest 3 posts

--- blog/views.py
+++ blog/views.py
@@ -65,18 +65,86 @@ posts = [
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
           velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
         """
+    },
+    {
+        "slug": "hike-in-the-mountains-2",
+        "image": "mountains.jpg",
+        "author": "Maximilian",
+        "date": date(2021, 8, 21),
+        "title": "Mountain Hiking - 2",
+        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "programming-is-fun-2",
+        "image": "coding.jpg",
+        "author": "Maximilian",
+        "date": date(2022, 4, 10),
+        "title": "Programming Is Great! - 2",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
+    },
+    {
+        "slug": "into-the-woods-2",
+        "image": "woods.jpg",
+        "author": "Maximilian",
+        "date": date(2020, 9, 5),
+        "title": "Nature At Its Best - 2",
+        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """
     }
 ]
 
 # Create your views here.
 def index(request):
-	return render(request, "blog/index.html", { "posts": posts })
+    posts.sort(key=lambda x: x["date"])
+    latest_posts = posts[-3:]
+    return render(request, "blog/index.html", {
+        "posts": latest_posts,
+        "has_posts": len(latest_posts) > 0
+    })
 
 
 def all_posts(request):
-	return render(request, "blog/all-posts.html", { "posts": posts })
+    return render(request, "blog/all-posts.html", { "posts": posts })
 
 
 def post_details(request, slug):
-	current_post = [post for post in posts if post["slug"] == slug][0]
-	return render(request, "blog/post-details.html", { "post": current_post })
+    current_post = [post for post in posts if post["slug"] == slug][0]
+    return render(request, "blog/post-details.html", { "post": current_post })
