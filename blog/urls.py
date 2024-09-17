6a84a6a31f50d4b96370c481223bf287f0105948
stzekov <stanislav.tzekov@dxc.com>
2024-09-17 10:47:50 +0300

    Add names to url's path functions

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path("", views.index),
-	path("posts", views.all_posts),
-	path("<slug>/post_details", views.post_details)
+	path("", views.index, name="index-page"),
+	path("posts", views.all_posts, name="all-posts-page"),
+	path("<slug>/post_details", views.post_details, name="post-details-page")
 ]
