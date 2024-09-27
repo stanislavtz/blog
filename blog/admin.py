658209fb9498db3ada1c08b69495bc78c80ef11f
stzekov <stanislav.tzekov@dxc.com>
2024-09-27 15:10:24 +0300

    Add PostAdmin class holing listing and filtering criterias

--- blog/admin.py
+++ blog/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from .models import Post, Author, Tag
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+	prepopulated_fields = {
+		"slug": ("title",)
+	}
 
-admin.site.register(Post)
+	list_display = ("title", "id", "date", "author")
+	list_filter = ("title", "author", "date", "tags")
+
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
