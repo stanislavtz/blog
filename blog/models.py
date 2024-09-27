2f1da5875307a6394cc38d47445e4cc6263f17b0
stzekov <stanislav.tzekov@dxc.com>
2024-09-27 14:19:31 +0300

    Add Tag model and Update Post model

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,23 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-	first_name = models.CharField(max_length=80)
-	last_name = models.CharField(max_length=80)
+	first_name = models.CharField(max_length=100)
+	last_name = models.CharField(max_length=100)
 	e_mail = models.EmailField(max_length=250)
 
 
 class Post(models.Model):
-	slug = models.CharField(max_length=100)
-	image = models.ImageField()
-	author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts")
-	date = models.DateField()
-	title = models.CharField(max_length=100)
-	excerpt = models.TextField()
-	content = models.TextField()
+	title = models.CharField(max_length=150)
+	excerpt = models.CharField(max_length=200)
+	image_name = models.CharField(max_length=100)
+	date = models.DateField(auto_now=True)
+	slug = models.SlugField(unique=True)
+	content = models.TextField(validators=[MinLengthValidator(10)])
+	author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name="posts")
+
+
+class Tag(models.Model):
+	caption = models.CharField(50)
 
