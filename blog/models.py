b9461d0380ab30dc58565ddf57745e670e11b97d
stzekov <stanislav.tzekov@dxc.com>
2024-09-27 14:03:28 +0300

    Add Post model

--- blog/models.py
+++ blog/models.py
@@ -7,5 +7,12 @@ class Author(models.Model):
 	e_mail = models.EmailField(max_length=250)
 
 
-
+class Post(models.Model):
+	slug = models.CharField(max_length=100)
+	image = models.ImageField()
+	author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="posts")
+	date = models.DateField()
+	title = models.CharField(max_length=100)
+	excerpt = models.TextField()
+	content = models.TextField()
 
