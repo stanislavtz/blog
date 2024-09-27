435bed4d715a8b372a3f5771288b8d29149c3cd7
stzekov <stanislav.tzekov@dxc.com>
2024-09-27 13:59:51 +0300

    Add Author model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+	first_name = models.CharField(max_length=80)
+	last_name = models.CharField(max_length=80)
+	e_mail = models.EmailField(max_length=250)
+
+
+
+
