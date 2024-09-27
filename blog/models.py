b038c52dbb25e82c9c70d9eacac343a6eef860cd
stzekov <stanislav.tzekov@dxc.com>
2024-09-27 14:48:58 +0300

    Update str methods to Tag and Author models

--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,20 @@ from django.db import models
 class Tag(models.Model):
 	caption = models.CharField(max_length=20)
 
+	def __str__(self):
+		return self.caption
+
 
 class Author(models.Model):
 	first_name = models.CharField(max_length=100)
 	last_name = models.CharField(max_length=100)
 	e_mail = models.EmailField(max_length=250)
+
+	def full_name(self):
+		return f"{self.first_name} {self.last_name}"
+
+	def __str__(self):
+		return self.full_name()
 
 
 class Post(models.Model):
