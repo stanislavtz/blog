e41c5ddefbb47013a4cbec41503512b006948563
stzekov <stanislav.tzekov@dxc.com>
2024-09-28 09:25:41 +0300

    Add url to author details page

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
 	path("", views.index, name="index-page"),
 	path("posts", views.get_all_posts, name="all-posts-page"),
-	path("posts/<slug:slug>", views.post_details, name="post-details-page")
+	path("posts/<slug:slug>", views.post_details, name="post-details-page"),
+	path("author/<id>", lambda x: x, name="author-details-page")
 ]
