a2532ee696aa494cf3f50031007ee2605d2a777f
stzekov <stanislav.tzekov@dxc.com>
2024-10-07 10:04:33 +0300

    Add Comment model

--- blog/models.py
+++ blog/models.py
@@ -30,3 +30,10 @@ class Post(models.Model):
 	content = models.TextField(validators=[MinLengthValidator(10)])
 	author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name="posts")
 	tags = models.ManyToManyField(Tag)
+
+
+class Comment(models.Model):
+	user_name = models.CharField(max_length=120)
+	e_mil = models.EmailField()
+	text = models.TextField(validators=[MinLengthValidator(10)], max_length=400)
+	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
