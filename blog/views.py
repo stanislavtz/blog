96f12cbec52221425b53c6a3d7c1a692c1e34cac
stzekov <stanislav.tzekov@dxc.com>
2024-10-07 16:03:51 +0300

    Refactor PostDetailsView

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
+from django.views import View
 from django.views.generic import ListView, DetailView
 
-from .models import Post, Author
+from .models import Post, Author, Comment
 from .forms import CommentForm
 
 # Create your views here.
@@ -23,15 +27,42 @@ class AllPostsView(ListView):
     template_name = "blog/all-posts.html"
 
 
-class PostDetailsView(DetailView):
-    model = Post
+class PostDetailsView(View):
     template_name = "blog/post-details.html"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data()
-        context["tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "tags": post.tags.all(),
+            "post_comments": post.comments.all(),
+            "comment_form": CommentForm()
+        }
+        return render(request, self.template_name, context)
+
+    def post(self, request, slug):
+        form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            new_comment = Comment(
+                user_name=data["user_name"],
+                e_mail=data["e_mail"],
+                text=data["text"],
+                post=post
+            )
+            new_comment.save()
+            return HttpResponseRedirect(reverse("post-details-page", args=[slug]))
+
+        context = {
+            "post": post,
+            "tags": post.tags.all(),
+            "post_comments": post.comments.all(),
+            "comment_form": form
+        }
+
+        return render(request, self.template_name, context)
 
 
 class AuthorDetailsView(DetailView):
