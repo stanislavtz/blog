e9d25f51072ec774e74a11bebbd32682fb1aa640
stzekov <stanislav.tzekov@dxc.com>
2024-10-07 08:59:54 +0300

    Add tags to PostDetailsView context

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,6 @@ class HomePageView(ListView):
     context_object_name = "posts"
     template_name = "blog/index.html"
 
-
     def get_queryset(self):
         query = super().get_queryset()[:3]
         return query
@@ -18,13 +17,19 @@ class HomePageView(ListView):
 
 class AllPostsView(ListView):
     model = Post
-    template_name = "blog/all-posts.html"
+    ordering = ["date"]
     context_object_name = "posts"
+    template_name = "blog/all-posts.html"
 
 
 class PostDetailsView(DetailView):
     model = Post
     template_name = "blog/post-details.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context["tags"] = self.object.tags.all()
+        return context
 
 
 class AuthorDetailsView(DetailView):
